0c1e3e4a27f82e7188503f7b44eb5c46915192b0
Anton Otaner <antonethem@gmail.com>
2021-05-08 14:53:51 -0400

    Fix ErrorMessage file

--- src/components/Sections/ErrorMessage.js
+++ src/components/Sections/ErrorMessage.js
@@ -4,7 +4,6 @@ import React, { useContext } from "react";
 
 //Components
 import Text from "../UI/Text";
-import CombinedRating from "../UI/CombinedRating";
 
 import { Context } from "../Layout/Context/Context";
 
